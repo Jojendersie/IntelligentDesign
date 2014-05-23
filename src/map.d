b4e56ec4d795e0b215b3219c9d0b35f1c41d5700
Johannes Jendersie <jojendersie@web.de>
2014-05-24 00:28:23 +0200

    position-random generator value noise map ground

--- src/map.d
+++ src/map.d
@@ -1,5 +1,12 @@
+import utils;
+
 class Map
 {
+	this()
+	{
+		generateGround();
+	}
+
 	bool isLand(float x, float y)
 	{
 		return sampleGround(x, y) > 0.0f;
@@ -24,5 +31,24 @@ private:
 		assert(iy >= 0 && iy < 99);
 		return (m_ground[ix][iy] * (1.0f - fx) + m_ground[ix+1][iy] * fx) * (1.0f - fy) +
 			   (m_ground[ix][iy+1] * (1.0f - fx) + m_ground[ix+1][iy+1] * fx) * fy;
+	}
+
+	void generateGround()
+	{
+		for( int x = 0; x < 100; ++x )
+		{
+			for( int y = 0; y < 100; ++y )
+			{
+				// Generate a value noise value
+				float value = 0.0f;
+				for( int oct = 0; oct < 5; ++oct )
+				{
+					// The randomAt-z component can be used to change the landscape over time
+					int scale = 1 << oct;
+					value += randomAt(x * 0.01f * scale, y * 0.01f * scale, 1.0f) / scale;
+				}
+				m_ground[x][y] = value;
+			}
+		}
 	}
 }
---
+++ src/utils.d
@@ -0,0 +1,37 @@
+float lerp(float a, float b, float t)
+{
+	return a * (1 - t) + b * t;
+}
+
+// Compute a "random" value at a position in a static white
+// noise with values in [0,1].
+// Sampling the same position will return the same value.
+//
+// To create 1D or 2D samples just set the other dimensions to
+// a constant value.
+float randomAt( int x, int y, int z )
+{
+	// Use states of xorshift to apply seeding
+	x ^= 0x7B4E10DC; y ^= 0x2D7C0472; z ^= 0x6B055F1D;
+
+	int value = (x * 0x9E3719B1 - (z >> 17))
+		^ (y * 0xAFFE3141 - (x >> 17))
+		^ (z * 0x27f161e8 - (y >> 17));
+
+	return value * 2.328306437e-10f;
+}
+
+// Sample the other randomAt-function with trilinear interpolation.
+float randomAt( float x, float y, float z )
+{
+	int ix = cast(int)x;
+	int iy = cast(int)y;
+	int iz = cast(int)z;
+	float fx = x - ix;
+	float fy = y - iy;
+	float fz = z - iz;
+	return lerp(lerp(lerp(randomAt(ix,iy,iz), randomAt(ix+1,iy,iz), fx),
+					 lerp(randomAt(ix,iy+1,iz), randomAt(ix+1,iy+1,iz), fx), fy),
+				lerp(lerp(randomAt(ix,iy,iz+1), randomAt(ix+1,iy,iz+1), fx),
+					 lerp(randomAt(ix,iy+1,iz+1), randomAt(ix+1,iy+1,iz+1), fx), fy), fz);
+}
